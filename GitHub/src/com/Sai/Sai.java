a95fdb6e88fdf4ae9013cf1e319c59cd25a85cc4
MORASAIKRISHNA <saikrishnamora4@gmail.com>
2023-12-29 13:19:42 +0530

    ok

--- GitHub/src/com/Sai/Sai.java
+++ GitHub/src/com/Sai/Sai.java
@@ -5,7 +5,7 @@ public class Sai {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int age=24;
+		int age=25;
 		String name="saikrishna";
 		System.out.println("the age:"+age);
 		
